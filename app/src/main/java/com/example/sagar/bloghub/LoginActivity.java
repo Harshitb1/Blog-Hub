e4b7c0572bafa7ea015df255590bff4b4c3f053e
Harshitb1 <harshitbatra56@yahoo.in>
2018-06-12 12:16:37 +0530

    login page

--- app/src/main/java/com/example/sagar/bloghub/LoginActivity.java
+++ app/src/main/java/com/example/sagar/bloghub/LoginActivity.java
@@ -1,15 +1,80 @@
 package com.example.sagar.bloghub;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText password, email;
+    Button login , signup;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth=FirebaseAuth.getInstance();
+        email= findViewById(R.id.emailEditext);
+        login= findViewById(R.id.loginButton);
+        password= findViewById(R.id.PasswordEdittext);
+        signup= findViewById(R.id.signupButton);
+        progressBar = findViewById(R.id.loginprogressBar);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String loginEmail= email.getText().toString();
+                String loginPassword= password.getText().toString();
+
+                if(!TextUtils.isEmpty(loginEmail)&&!TextUtils.isEmpty(loginPassword)){
+                    progressBar.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(loginEmail,loginPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                sendToMain();
+                            }
+                            else{
+                                String error= task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, error, Toast.LENGTH_SHORT).show();
+                            }
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser= mAuth.getCurrentUser();
+        if(currentUser!=null){
+           sendToMain();
+        }
+    }
+
+    private void sendToMain() {
+        Intent mainIntent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(mainIntent);
+        finish();
     }
 }
--- app/src/main/java/com/example/sagar/bloghub/MainActivity.java
+++ app/src/main/java/com/example/sagar/bloghub/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-        startActivity(intent);
     }
 
     @Override
