863b45b49aceae4e5e3915c64140eb78ee9c3534
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2018-06-20 02:26:09 +0530

    postActivity

--- app/src/main/java/com/example/sagar/bloghub/MainActivity.java
+++ app/src/main/java/com/example/sagar/bloghub/MainActivity.java
@@ -1,27 +1,49 @@
 package com.example.sagar.bloghub;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     FirebaseAuth mAuth;
+    FloatingActionButton addPostButton;
+    FirebaseFirestore firebaseFirestore;
+    String currentUserId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth= FirebaseAuth.getInstance();
+        firebaseFirestore= FirebaseFirestore.getInstance();
         toolbar= findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Blog Hub");
+        addPostButton=findViewById(R.id.addPostButton);
+
+        addPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent= new Intent(MainActivity.this,NewPostActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -31,7 +53,26 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser= FirebaseAuth.getInstance().getCurrentUser();
         if(currentUser== null){
            sendToLogin();
+        }else{
+            currentUserId= mAuth.getCurrentUser().getUid();
+            firebaseFirestore.collection("Users").document(currentUserId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if(task.isSuccessful()){
+                        if(!task.getResult().exists()){
+                            Intent intent = new Intent(MainActivity.this, SetupActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+
+                    }else {
+                        String error = task.getException().getMessage();
+                        Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
+
     }
 
     @Override
