03f9c7991ebd6ec66a8865d30ae92f98cc13be12
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2018-06-28 00:31:31 +0530

    pagination

--- app/src/main/java/com/example/sagar/bloghub/BlogRecyclerAdapter.java
+++ app/src/main/java/com/example/sagar/bloghub/BlogRecyclerAdapter.java
@@ -9,10 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Date;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import static java.lang.String.format;
 
@@ -20,6 +29,7 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
 
     List<BlogPost> list;
     Context context;
+    FirebaseFirestore firebaseFirestore;
 
     BlogRecyclerAdapter(List<BlogPost> list){
         this.list=list;
@@ -30,18 +40,40 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
     public BlogRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         context= parent.getContext();
+        firebaseFirestore=FirebaseFirestore.getInstance();
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_list_item,parent,false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BlogRecyclerAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final BlogRecyclerAdapter.ViewHolder holder, int position) {
 
         String descData= list.get(position).getDesc();
         holder.setDescText(descData);
 
         String image_url = list.get(position).getImage_url();
-        holder.setBlogImage(image_url);
+        String thumb_uri = list.get(position).getImage_thumb();
+        holder.setBlogImage(image_url,thumb_uri);
+
+
+        String userId= list.get(position).getUser_id();
+        firebaseFirestore.collection("Users").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if(task.isSuccessful()){
+
+                    String userName= task.getResult().getString("name");
+                    String userImage= task.getResult().getString("image");
+                    holder.setUserData(userName,userImage);
+
+                }else{
+
+                    String error= task.getException().getMessage();
+                    Toast.makeText(context, error, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         long milliseconds =list.get(position).getTimestamp().getTime();
         String dateString = DateFormat.format("MM/dd/yyyy", new Date(milliseconds)).toString();
@@ -60,6 +92,8 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
         View view;
         TextView dateTextView;
         ImageView blogImageView;
+        TextView usernameTextView;
+        CircleImageView circleImageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -71,16 +105,25 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
             descTextView.setText(text);
         }
 
-        public void setBlogImage(String downloadUri){
+        public void setBlogImage(String downloadUri,String thuhmbUri){
 
             blogImageView=view.findViewById(R.id.postImageView);
-            Glide.with(context).load(downloadUri).into(blogImageView);
+            Glide.with(context).load(downloadUri).thumbnail(Glide.with(context).load(thuhmbUri)).into(blogImageView);
         }
 
         public void  setTime(String date){
 
             dateTextView=view.findViewById(R.id.timeTextView);
             dateTextView.setText(date);
+        }
+
+        public void setUserData(String name, String image){
+
+            usernameTextView=view.findViewById(R.id.usernameTextView);
+            usernameTextView.setText(name);
+
+            circleImageView= view.findViewById(R.id.profileImageView);
+            Glide.with(context).load(image).into(circleImageView);
         }
     }
 }
--- app/src/main/java/com/example/sagar/bloghub/HomeFragment.java
+++ app/src/main/java/com/example/sagar/bloghub/HomeFragment.java
@@ -8,11 +8,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -29,6 +33,9 @@ public class HomeFragment extends Fragment {
     List<BlogPost> blogList;
     FirebaseFirestore firebaseFirestore;
     BlogRecyclerAdapter adapter;
+    FirebaseAuth firebaseAuth;
+    DocumentSnapshot lastVisible;
+    Boolean isFirstPageLoadedFirst=true;
 
 
     public HomeFragment() {
@@ -44,29 +51,83 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         blogList = new ArrayList<>();
         recyclerView= view.findViewById(R.id.postRecyclerView);
-        firebaseFirestore= FirebaseFirestore.getInstance();
         adapter= new BlogRecyclerAdapter(blogList);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
 
-        firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser()!=null ){
+            firebaseFirestore = FirebaseFirestore.getInstance();
+
+            recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+
+                    Boolean isReachedBottom = recyclerView.canScrollVertically(1);
+
+                    if(!isReachedBottom){
+                       //Toast.makeText(getContext(),"Pagination",Toast.LENGTH_SHORT).show();
+                        loadMorePost();
+                    }
+                }
+            });
+            Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp",Query.Direction.DESCENDING).limit(4);
+            firstQuery.addSnapshotListener(getActivity(),new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+
+                    if(isFirstPageLoadedFirst) {
+                        lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
+                    }
+                    for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+
+                        if (doc.getType() == DocumentChange.Type.ADDED) {
+
+                            BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
+                            if(isFirstPageLoadedFirst) {
+                                blogList.add(blogPost);
+                            }else {
+                                blogList.add(0,blogPost);
+                            }
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                    isFirstPageLoadedFirst=false;
+                }
+            });
+        }
+        return view;
+    }
+
+    public void loadMorePost(){
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        Query nextQuery = firebaseFirestore.collection("Posts")
+                .orderBy("timestamp",Query.Direction.DESCENDING)
+                .startAfter(lastVisible)
+                .limit(4);
+
+        nextQuery.addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
 
-                for(DocumentChange doc : documentSnapshots.getDocumentChanges()){
+                if(!documentSnapshots.isEmpty()) {
+                    lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
+                    for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
 
-                    if(doc.getType()== DocumentChange.Type.ADDED){
+                        if (doc.getType() == DocumentChange.Type.ADDED) {
 
-                        BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
-                        blogList.add(blogPost);
-                        adapter.notifyDataSetChanged();
+                            BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
+                            blogList.add(blogPost);
+                            adapter.notifyDataSetChanged();
+                        }
                     }
                 }
             }
         });
-
-        return view;
     }
-
 }
